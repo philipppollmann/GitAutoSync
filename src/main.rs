f30c6dc2c025215e2eb2a564a3aebf8b79c080d1
Philipp <philipp@geuer-pollmann.de>
2024-05-02 18:05:12 +0200

    feat: implemented function to sleep in loop and put git commands to own module

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,24 @@
-mod git_commands;
+use std::thread;
+use std::time::Duration;
+
+
 mod message_generator;
+mod project_folders;
+mod git_commands;
 
 
 fn main() {
-    let message = message_generator::generate_message();
-    format!("{}", message);
-}
+        loop {
+            let message = message_generator::generate_message();
+            println!("{}", message);
+
+            let project_list = project_folders::get_project_list();
+
+            for project in project_list {
+                println!("{}", project);
+            }
+
+            thread::sleep(Duration::from_secs(3600));
+        }
+
+    }
---
+++ src/project_folders.rs
@@ -0,0 +1,6 @@
+pub(crate) fn get_project_list() -> Vec<String> {
+    let mut projects = Vec::new();
+    projects.push("/Users/philipppollmann/Library/Mobile Documents/iCloud~md~obsidian/Documents/Obsidian".to_string());
+    projects.push("/Users/philipppollmann/Library/Mobile Documents/iCloud~com~logseq~logseq/Documents/Logseq".to_string());
+    projects
+}
