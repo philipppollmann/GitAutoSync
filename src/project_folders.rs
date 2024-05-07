50b77700eddfa4a0cf8631c3eab72871168f054f
Philipp <philipp.pollmann@telekom.de>
2024-05-07 12:18:26 +0200

    feat: changed obsidian to logseq

--- src/project_folders.rs
+++ src/project_folders.rs
@@ -5,7 +5,7 @@ pub fn get_project_list() -> Vec<PathBuf> {
     let mut projects = Vec::new();
     projects.push(PathBuf::from("/Users/philipppollmann/Library/Mobile Documents/iCloud~md~obsidian/Documents/Obsidian"));
     projects.push(PathBuf::from("/Users/philipppollmann/Library/Mobile Documents/iCloud~com~logseq~logseq/Documents/Logseq"));
-    projects.push(PathBuf::from(r"C:\github\Obsidian"));
+    projects.push(PathBuf::from(r"C:\github\Logseq"));
     projects.push(PathBuf::from(r"C:\github\Obsidian"));
     projects.push(PathBuf::from(r"/Users/A200017104/GitHub/Obsidian"));
     projects.push(PathBuf::from(r"/Users/A200017104/GitHub/Logseq"));
